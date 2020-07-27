7a66b9b860c71d89c6d64eadc620a46ff4a28ec1
n-tanoDC <nicolas.tano42@gmail.com>
2020-07-27 13:11:11 +0200

    Réorganisation du projet

---
+++ app/App.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { Text, TextInput, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+export default function App() {
+  return (<Text>App</Text>)
+}
