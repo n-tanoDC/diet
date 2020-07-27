f7fcd19b33088427986eab9b4b0f8080e088e6e8
n-tanoDC <nicolas.tano42@gmail.com>
2020-07-27 14:57:59 +0200

    Création d'un composant carte pour le HomeScreen

---
+++ app/components/MealSection.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, Card, Icon, Button } from '@ui-kitten/components';
+
+export default (props) => {
+
+  const Header = (cardProps) => (
+    <View {...cardProps} style={[styles.header, cardProps.style]}>
+      <Text category="h6">
+        {props.title}
+      </Text>
+      <Icon
+        name='plus-circle-outline'
+        fill='grey'
+        style={styles.icon}
+        onPress={() => props.navigate('Search')} />
+    </View>
+  )
+  return (
+    <Card styles={styles.card} header={Header}>
+      <Text>Aucun aliment ajouté.</Text>
+    </Card>
+  )
+}
+
+const styles = StyleSheet.create({
+  card: {
+    padding: 2
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 5,
+  },
+  icon: {
+    width: 40,
+    height: 40,
+  }
+})
