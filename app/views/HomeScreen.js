a7ee8dd886a53548997de76e930a94a1571ef9d9
n-tanoDC <nicolas.tano42@gmail.com>
2020-07-27 15:52:12 +0200

    Ajout d'une section par repas

--- app/views/HomeScreen.js
+++ app/views/HomeScreen.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import { Button, Divider, Layout, TopNavigation } from '@ui-kitten/components';
+import { Divider, Layout, TopNavigation, Button } from '@ui-kitten/components';
 import MealSection from '../components/MealSection';
 
+const mealSections = [
+  "Petit Déjeuner", "Déjeuner", "Diner"
+]
+
 export default ({ navigation }) => {
+
+  const renderedSections = mealSections.map((section, index) => <MealSection key={index} title={section} navigate={navigation.navigate}/>) 
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <TopNavigation title="Aujourd'hui" alignment='center'/>
       <Divider/>
       <Layout style={{ padding: 10 }}>
-        <MealSection title="Petit déjeuner" navigate={navigation.navigate}/>
+        {renderedSections}
+        {/* // Todo => Créer et rendre un composant Summary */}
         <Button onPress={() => navigation.navigate('Search')}>Recherche</Button>
       </Layout>
     </SafeAreaView>
