60511f7d76580f4cf918938c68c1b590419832cc
n-tanoDC <nicolas.tano42@gmail.com>
2020-07-27 13:16:24 +0200

    Initialisation de la navigation

--- app/App.js
+++ app/App.js
@@ -1,8 +1,18 @@
-import React, { useState } from 'react';
-import { Text, TextInput, Image } from 'react-native';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './views/HomeScreen';
+import SearchScreen from './views/SearchScreen';
 
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (<Text>App</Text>)
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='Search' component={SearchScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
