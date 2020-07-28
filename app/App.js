c86a6c4007f80165e073e7460d723c7ab0074e51
n-tanoDC <nicolas.tano42@gmail.com>
2020-07-28 18:53:13 +0200

    Réorganisation du code

--- app/App.js
+++ app/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import AppNavigator from './AppNavigator';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './views/HomeScreen';
+import SearchScreen from './views/SearchScreen';
+
+const { Screen, Navigator } = createStackNavigator();
 
 export default () => {
   return (
-    <>
-      <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider {...eva} theme={eva.light}>
-        <AppNavigator />
-      </ApplicationProvider>
-    </>
+    <NavigationContainer>
+      <Navigator headerMode='none' initialRouteName='Home'>
+        <Screen name='Home' component={HomeScreen} />
+        <Screen name='Search' component={SearchScreen} />
+      </Navigator>
+    </NavigationContainer>
   );
 }
