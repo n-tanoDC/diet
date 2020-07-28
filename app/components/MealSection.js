c83c4e4a15b3a7dccb400888783f1543bf3ee167
n-tanoDC <nicolas.tano42@gmail.com>
2020-07-28 19:30:39 +0200

    Ajout d'un badge d'ajout + mise en forme

--- app/components/MealSection.js
+++ app/components/MealSection.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import { Card, CardItem, Text, Body } from 'native-base';
+import { Card, CardItem, Text, Body, Icon, Right, Button, Badge } from 'native-base';
+import { StyleSheet } from 'react-native';
 
 export default (props) => {
 
   return (
     <Card>
-      <CardItem header bordered>
+      <CardItem header bordered style={styles.cardHeader}>
         <Text>{props.title}</Text>
+        <Right>
+          <Badge primary>
+            <Icon 
+              onPress={() => props.navigate('Search')}
+              type="MaterialIcons"
+              name="add"
+              style={styles.icon} />
+          </Badge>
+        </Right>
       </CardItem>
       <CardItem>
-        <Body>
-          <Text>Aucun aliment ajouté.</Text>
-        </Body>
+        <Text>Aucun aliment ajouté.</Text>
       </CardItem>
     </Card>
   )
 }
+
+const styles = StyleSheet.create({
+  cardHeader: {
+    justifyContent: "space-between",
+  },
+  icon: {
+    fontSize: 15,
+    color: "#fff",
+    lineHeight: 20,
+  },
+})
