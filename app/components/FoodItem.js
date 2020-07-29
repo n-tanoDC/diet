abbb649592d14b2b239cd97912be279dbef7b560
n-tanoDC <nicolas.tano42@gmail.com>
2020-07-29 12:34:29 +0200

    Implémentation de la fonction removeData

--- app/components/FoodItem.js
+++ app/components/FoodItem.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { CardItem, Text, Right, Icon } from 'native-base';
 import { StyleSheet } from 'react-native';
+import { removeData } from '../functions';
 
 export default (props) => {
-  const { item } = props;
+  const { item, storageKey } = props;
+  const [deleteSuccess, setDeleteSuccess] = useState(false)
 
+  if (deleteSuccess) {
+    return (
+      <></>
+    )
+  }
+  
   return (
     <CardItem style={styles.cardItemContainer}>
       <Text>{item.food_name}</Text>
       <Right>
-        <Icon name="trash"/>
+        <Icon
+          name="trash"
+          onPress={() => removeData(storageKey, item, setDeleteSuccess)} />
       </Right>
     </CardItem>
   )
