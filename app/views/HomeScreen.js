9325de13b4193d05d6d9b90a2e3708e29c712082
n-tanoDC <nicolas.tano42@gmail.com>
2020-07-29 14:15:41 +0200

    Génération d'une storage key unique

--- app/views/HomeScreen.js
+++ app/views/HomeScreen.js
@@ -1,39 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MealSection from '../components/MealSection';
 import { Container, Content } from 'native-base';
 import CustomHeader from '../components/CustomHeader';
 import { StyleSheet } from 'react-native';
+import Summary from '../components/Summary';
 
-const mealSections = [
-  {
-    title: "Petit déjeuner",
-    storage_key: "@save-breakfast"
-  },
-  {
-    title: "Déjeuner",
-    storage_key: "@save-lunch"
-  },
-  {
-    title: "Dîner",
-    storage_key: "@save-diner"
-  }
-]
+const mealSections = ["Petit déjeuner", "Déjeuner", "Dîner"];
+const STORAGE_KEY = "@save-item"
 
 export default ({ navigation }) => {
 
   const renderedSections = mealSections.map((section, index) => (
     <MealSection
       key={index}
-      storageKey={section.storage_key}
-      title={section.title}
+      storageKey={STORAGE_KEY}
+      title={section}
       navigate={navigation.navigate}/>
   )) 
+
+  const [data, setData] = useState([]);
+
+  
+  
 
   return (
     <Container>
       <CustomHeader title="Aujourd'hui" left="home" />
       <Content style={styles.contentContainer}>
         {renderedSections}
+        <Summary />
       </Content>
     </Container>
   );
