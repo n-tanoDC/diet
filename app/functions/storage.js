3c3c7cf3363623b32e1156069dc462fd07b192d1
n-tanoDC <nicolas.tano42@gmail.com>
2020-07-30 16:11:18 +0200

    Ajout de fonction AsyncStorage pour l'user

--- app/functions/storage.js
+++ app/functions/storage.js
@@ -1,15 +1,16 @@
 import AsyncStorage from '@react-native-community/async-storage';
 
-const STORAGE_KEY = "@save-item"
+const FOOD_KEY = "@save-item";
+const USER_KEY = "@save-user";
 
 export const clearItems = (callback) => {
   let emptyValue = JSON.stringify([]);
-  AsyncStorage.setItem(STORAGE_KEY, emptyValue)
+  AsyncStorage.setItem(FOOD_KEY, emptyValue)
     .then(() => callback([]))
 }
 
 export const readItems = (callback) => {
-  AsyncStorage.getItem(STORAGE_KEY)
+  AsyncStorage.getItem(FOOD_KEY)
   .then(res => JSON.parse(res))
   .then(data => {
     callback(data)
@@ -21,12 +22,12 @@ export const readItems = (callback) => {
 
 export const addItem = (value, setAdded) => {
   let newData;
-  AsyncStorage.getItem(STORAGE_KEY)
+  AsyncStorage.getItem(FOOD_KEY)
   .then(res => JSON.parse(res))
   .then(data => newData = data ? [value, ...data] : [value])
   .then(() => {
     newData = JSON.stringify(newData);
-    AsyncStorage.setItem(STORAGE_KEY, newData);
+    AsyncStorage.setItem(FOOD_KEY, newData);
     setAdded(true);
   })
   .catch(err => {
@@ -36,7 +37,7 @@ export const addItem = (value, setAdded) => {
 
 export const removeItem = (value, deleted) => {
   let newData;
-  AsyncStorage.getItem(STORAGE_KEY)
+  AsyncStorage.getItem(FOOD_KEY)
     .then(res => JSON.parse(res))
     .then(data => {
       const index = data.findIndex(element => element.food_name === value.food_name);
@@ -45,10 +46,27 @@ export const removeItem = (value, deleted) => {
     })
   .then(() => {
     newData = JSON.stringify(newData);
-    AsyncStorage.setItem(STORAGE_KEY, newData);
+    AsyncStorage.setItem(FOOD_KEY, newData);
     deleted(true)
   })
   .catch(err => {
     console.log(err);
   });
+}
+
+export const saveUser = (value, callback) => {
+  const userValue = JSON.stringify(value);
+  AsyncStorage.setItem(USER_KEY, userValue)
+    .then(() => callback(true))
+}
+
+export const getUser = (callback) => {
+  AsyncStorage.getItem(USER_KEY)
+    .then(res => JSON.parse(res))
+    .then(data => {
+      callback(data)
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
