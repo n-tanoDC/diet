d5d972491716faa160dabca3a01e28f3e36cd6cd
n-tanoDC <nicolas.tano42@gmail.com>
2020-07-30 23:45:16 +0200

    Test de la connexion avant de fetch les données

--- app/functions/data.js
+++ app/functions/data.js
@@ -1,23 +1,30 @@
 import { Alert } from "react-native";
+import NetInfo from "@react-native-community/netinfo";
 
 const requestHeaders = {
   "x-app-id": "ff0ccea8",
   "x-app-key": "605660a17994344157a78f518a111eda",
 }
 
-export const fetchData = (input, callback, loading) => {
-  loading(true)
-  fetch('https://trackapi.nutritionix.com/v2/search/instant?query=' + input + '&locale=fr_FR&detailed=true', {
-    headers: requestHeaders
-  })
-    .then(res => res.json())
-    .then(data => {
-      callback(data.common);
-      loading(false); 
-    })
-    .catch(err => {
-      Alert.alert("connexion à la base de données impossible.\nVeuillez réessayer ultérieurement.")
-      console.log(err);
-      loading(false);
-    })
+export const fetchData = async (input, callback, loading) => {
+    const connexion = await NetInfo.fetch()
+    if (connexion.isConnected && connexion.isInternetReachable) {
+      console.log("is connected");
+      loading(true);
+      fetch('https://trackapi.nutritionix.com/v2/search/instant?query=' + input + '&locale=fr_FR&detailed=true', {
+        headers: requestHeaders
+      })
+        .then(res => res.json())
+        .then(data => {
+          callback(data.common);
+          loading(false); 
+        })
+        .catch(err => {
+          console.log(err);
+          loading(false);
+        })
+    } else {
+      Alert.alert("Pas de connexion internet");
+    }
+  
 }
